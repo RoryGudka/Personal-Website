e0d5e4721fad405ae0589faad5f377448f8b16f4
Rory <rdgudka@gmail.com>
2024-04-13 14:43:28 -0400

    Unify backgrounds

--- components/Footer/FooterBackground.tsx
+++ components/Footer/FooterBackground.tsx
@@ -1,6 +1,4 @@
-import { Box } from "@mui/material";
 import GreenSection from "../GreenSection";
-import Particles from "react-tsparticles";
 import { ReactNode } from "react";
 
 interface Props {
@@ -9,93 +7,7 @@ interface Props {
 
 const FooterBackground: React.FC<Props> = ({ children }) => {
   return (
-    <GreenSection onlyClipTop>
-      <Box width="100%" height="100%" overflow="hidden">
-        <Box
-          position="absolute"
-          top={0}
-          left={0}
-          height="100%"
-          ml="-10%"
-          mt="-10%"
-          sx={{ "#footer-particles": { height: "100%" } }}
-        >
-          <Particles
-            id="footer-particles"
-            width="120%"
-            height="120%"
-            options={{
-              fullScreen: false,
-              fps_limit: 30,
-              particles: {
-                number: {
-                  value: 80,
-                  density: {
-                    enable: true,
-                    value_area: 800,
-                  },
-                },
-                color: { value: "#ffffff" },
-                shape: { type: "circle" },
-                opacity: {
-                  value: 0.3,
-                  random: false,
-                  anim: {
-                    enable: false,
-                    speed: 1,
-                    opacity_min: 0.1,
-                    sync: false,
-                  },
-                },
-                size: {
-                  value: 3,
-                  random: true,
-                  anim: {
-                    enable: false,
-                    speed: 40,
-                    size_min: 0.1,
-                    sync: false,
-                  },
-                },
-                line_linked: {
-                  enable: true,
-                  distance: 150,
-                  color: "#ffffff",
-                  opacity: 0.4,
-                  width: 1,
-                },
-                move: {
-                  enable: true,
-                  speed: 0.25,
-                  direction: "none",
-                  random: false,
-                  straight: false,
-                  out_mode: "out",
-                  bounce: false,
-                  attract: {
-                    enable: false,
-                    rotateX: 600,
-                    rotateY: 1200,
-                  },
-                },
-              },
-              interactivity: {
-                detect_on: "canvas",
-                events: {
-                  onhover: { enable: true, mode: "grab" },
-                  onclick: { enable: true, mode: "push" },
-                  resize: false,
-                },
-                modes: {
-                  grab: { distance: 140, line_linked: { opacity: 1 } },
-                  push: { particles_nb: 1 },
-                },
-              },
-              retina_detect: true,
-            }}
-          />
-        </Box>
-      </Box>
+    <GreenSection id="footer" onlyClipTop>
       {children}
     </GreenSection>
   );
--- components/GreenSection.tsx
+++ components/GreenSection.tsx
@@ -1,17 +1,112 @@
 import { Box } from "@mui/material";
+import Particles from "react-tsparticles";
 import { ReactNode } from "react";
 
 interface Props {
+  id: string;
   children: ReactNode;
   onlyClipBottom?: boolean;
   onlyClipTop?: boolean;
 }
 
 const GreenSection: React.FC<Props> = ({
+  id,
   children,
   onlyClipBottom,
   onlyClipTop,
 }) => {
+  const subnode = (
+    <>
+      <Box width="100%" height="100%" overflow="hidden">
+        <Box
+          position="absolute"
+          top={0}
+          left={0}
+          height="100%"
+          ml="-10%"
+          mt="-10%"
+          sx={{ [`#${id}`]: { height: "100%" } }}
+        >
+          <Particles
+            id={id}
+            width="120%"
+            height="120%"
+            options={{
+              fullScreen: false,
+              fps_limit: 30,
+              particles: {
+                number: {
+                  value: 80,
+                  density: {
+                    enable: true,
+                    value_area: 800,
+                  },
+                },
+                color: { value: "#ffffff" },
+                shape: { type: "circle" },
+                opacity: {
+                  value: 0.3,
+                  random: false,
+                  anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false,
+                  },
+                },
+                size: {
+                  value: 3,
+                  random: true,
+                  anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false,
+                  },
+                },
+                line_linked: {
+                  enable: true,
+                  distance: 150,
+                  color: "#ffffff",
+                  opacity: 0.4,
+                  width: 1,
+                },
+                move: {
+                  enable: true,
+                  speed: 0.25,
+                  direction: "none",
+                  random: false,
+                  straight: false,
+                  out_mode: "out",
+                  bounce: false,
+                  attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200,
+                  },
+                },
+              },
+              interactivity: {
+                detect_on: "canvas",
+                events: {
+                  onhover: { enable: true, mode: "grab" },
+                  onclick: { enable: true, mode: "push" },
+                  resize: false,
+                },
+                modes: {
+                  grab: { distance: 140, line_linked: { opacity: 1 } },
+                  push: { particles_nb: 1 },
+                },
+              },
+              retina_detect: true,
+            }}
+          />
+        </Box>
+      </Box>
+      {children}
+    </>
+  );
+
   if (onlyClipBottom) {
     return (
       <Box
@@ -23,7 +118,7 @@ const GreenSection: React.FC<Props> = ({
           overflow: "hidden",
         }}
       >
-        {children}
+        {subnode}
         <Box height="100px" />
       </Box>
     );
@@ -38,7 +133,7 @@ const GreenSection: React.FC<Props> = ({
         }}
       >
         <Box height="100px" />
-        {children}
+        {subnode}
       </Box>
     );
   } else {
@@ -53,7 +148,7 @@ const GreenSection: React.FC<Props> = ({
         }}
       >
         <Box height="100px" />
-        {children}
+        {subnode}
         <Box height="100px" />
       </Box>
     );

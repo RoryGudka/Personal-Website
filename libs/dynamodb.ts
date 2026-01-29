a1821ddc6d1ed165d0f6b7979b0b745a8521e677
Rory <rdgudka@gmail.com>
2026-01-29 14:36:25 -0500

    Fix credentials issue

--- libs/dynamodb.ts
+++ libs/dynamodb.ts
@@ -1,6 +1,6 @@
+import AWS from "aws-sdk";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
-import AWS from "aws-sdk";
 
 AWS.config.update({
   region: process.env.AWS_REGION || "",
@@ -8,4 +8,12 @@ AWS.config.update({
   secretAccessKey: process.env.AWS_IAM_SECRET_ACCESS_KEY || "",
 });
 
-export const dynamodb = DynamoDBDocument.from(new DynamoDB());
+export const dynamodb = DynamoDBDocument.from(
+  new DynamoDB({
+    region: process.env.AWS_REGION || "",
+    credentials: {
+      accessKeyId: process.env.AWS_IAM_ACCESS_KEY_ID || "",
+      secretAccessKey: process.env.AWS_IAM_SECRET_ACCESS_KEY || "",
+    },
+  }),
+);
